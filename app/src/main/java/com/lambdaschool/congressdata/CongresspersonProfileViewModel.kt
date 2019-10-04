a1ef9d39103dafdaf31284b0cbc08584ce960964
Matthew Nelson <05nelsonm@protonmail.com>
2019-10-04 12:09:44 -0400

    fixed app crash upon clicking congressperson

--- app/src/main/java/com/lambdaschool/congressdata/CongresspersonProfileViewModel.kt
+++ app/src/main/java/com/lambdaschool/congressdata/CongresspersonProfileViewModel.kt
@@ -3,8 +3,11 @@ package com.lambdaschool.congressdata
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.ViewModel
 
-class CongresspersonProfileViewModel(var id: String?) : ViewModel() {
+class CongresspersonProfileViewModel : ViewModel() {
+
+    var id: String? = null
     var profile: LiveData<CongresspersonProfile>? = null
+
     get() {
         return if (field == null) {
             loadProfile()
